c91ca2f0e194c8d4d6b0979fa2e99e32bafa1b32
Ines <168848567+nessnos@users.noreply.github.com>
2026-09-19 21:17:40 +0200

    Delete pinlock.koplugin directory and keep only plugin files in root directory

--- pinlock.koplugin/_meta.lua
+++
@@ -1,5 +0,0 @@
-local _ = require("gettext")
-return {
-    fullname = _("PinLock"),
-    description = _([[Adds a PIN-code lock screen, styled after typical e-reader lock screens, that can require a PIN when KOReader starts and/or when the device wakes from sleep.]]),
-}
